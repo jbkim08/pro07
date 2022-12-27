05853b63e81ea627b601cf462adbef09770d38a4
eugene3367 <dbwls2518@naver.com>
2022-12-27 18:46:07 +0900

    pro07 sample

---
+++ pro07/pro07_1.sql
@@ -0,0 +1,9 @@
+create database goverment;
+use goverment;
+create table sample(id varchar(20), pw varchar(20));
+
+insert into sample values('keg','1234');
+insert into sample values('kea','123a');
+insert into sample values('keb','123b');
+insert into sample values('kec','123c');
+insert into sample values('ked','123d');
---
+++ pro07/src/main/java/kr/go/msit/service/SampleService.java
@@ -0,0 +1,12 @@
+package kr.go.msit.service;
+
+import java.util.List;
+
+import kr.go.msit.dto.SampleDTO;
+
+public interface SampleService {
+	public List<SampleDTO> sampleList() throws Exception;
+	public SampleDTO getSample(String id) throws Exception;
+	public void addSample(SampleDTO sample) throws Exception;
+
+}
